3e56ed32489d19395b23093b08445b4d55c5af04
rachmi00 <r.agyingi@gmail.com>
2026-01-24 13:50:07 +0100

    initialised class square to represent nodes in the algorithm

---
+++ algorithm.py
@@ -0,0 +1,34 @@
+import pygame
+import math
+from queue import PriorityQueue
+
+
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("A* Pathfinding Algorithm Visualization")
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+TURQUOISE = (64, 224, 208)
+
+class Sqaure:
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        #keep track of the actual x, y coordinates
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.neighbors = []
+        self.width = width
+        self.total_rows = total_rows
+    
+    def get_pos(self):
+        return self.row, self.col
