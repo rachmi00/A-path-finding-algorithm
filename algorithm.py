a326c219f2077227178f356137157b1da92c9146
rachmi00 <r.agyingi@gmail.com>
2026-01-25 21:44:19 +0100

    make grid

--- algorithm.py
+++ algorithm.py
@@ -82,3 +82,12 @@ def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
+
+def make_grid(rows, width):
+    grid  = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            square = Sqaure(i, j, gap, rows)
+            grid[i].append(square)
